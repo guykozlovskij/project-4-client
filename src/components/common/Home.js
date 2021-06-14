79f4c6f2980632e053a2507352b278c659a8eff7
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-06-14 09:57:36 +0100

    Connecting it to backend

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,4 +1,14 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+
 export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('/api/songs/')
+      console.log(response.data)
+    }
+    getData()
+  }, [])
   return (
     <h1>Home Page</h1>
   )
