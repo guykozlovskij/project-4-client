39cfbd3e78ac6fdf5fcd961c06a2a9900a51bb59
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2021-06-14 12:00:10 +0100

    register basic set-up

---
+++ src/lib/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+// import { getToken } from './auth'
+const baseUrl = '/api'
+
+// function headers() {
+//   return {
+//     headers: { Authorization: `Bearer ${getToken()}` },
+//   }
+// }
+
+//? Authentication requests
+export function registerUser(formData) {
+  return axios.post(`${baseUrl}/auth/register/`, formData)
+}
+
+export function loginUser(formData) {
+  return axios.post(`${baseUrl}/auth/login`, formData)
+}
