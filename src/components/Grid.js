a245b413df452dfed2bc8131507df4b29f2b70ec
Chris Wood <cawoodlfc@hotmail.co.uk>
2021-06-15 18:55:31 +0100

    removed the visual cue

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -14,14 +14,12 @@ export default function Grid() {
   const history = useHistory()
   const [isSaving, setIsSaving] = useState(false)
   let stepper = 0
-  const [whichBox, setWhichBox] = useState(stepper % 16)
+  // const [whichBox, setWhichBox] = useState(0)
   const transportEventId = useRef(null)
-  // const [songStarted, setSongStarted] = useState(false)
   const [numberOfActive, setNumberOfActive] = useState(0)
   const [allNotes, setAllNotes] = useState(noNotes)
   const gain = new Tone.Gain(0.1)
   gain.toDestination()
-
   const synths = new Tone.PolySynth().connect(gain)
 
   useEffect(() => {
@@ -37,7 +35,7 @@ export default function Grid() {
   const notes = Object.keys(allNotes)
   const repeat = (time) => {
     const step = stepper % 16
-    setWhichBox(step)
+    // setWhichBox(step)
     notes.forEach((note) => {
       if (allNotes[note][step]) {
         synths.triggerAttackRelease(note, '8n', time)
@@ -80,14 +78,13 @@ export default function Grid() {
     await Tone.Transport.clear(transportEventId.current)
 
   }
-
-  const handleSave = async() => {
+  const handleSave = async () => {
     await Tone.Transport.stop()
     await Tone.Transport.clear(transportEventId.current)
     setIsSaving(!isSaving)
     setIsPlaying(false)
   }
-  const handleSaveNotLoggedIn = async() => {
+  const handleSaveNotLoggedIn = async () => {
     setSavedSong(allNotes, numberOfActive)
     history.push('/login')
   }
@@ -97,7 +94,9 @@ export default function Grid() {
         <h1>Grid Stuff</h1>
         {notes.map(note => {
           return (
-            <IndividualButton key={note} note={note} buttonsSelected={allNotes[note]} setAllNotes={setAllNotes} allNotes={allNotes} setNumberOfActive={setNumberOfActive} numberOfActive={numberOfActive} isPlaying={isPlaying} synth={synths} step={whichBox}/>
+            <IndividualButton key={note} note={note} buttonsSelected={allNotes[note]} setAllNotes={setAllNotes} allNotes={allNotes} setNumberOfActive={setNumberOfActive} numberOfActive={numberOfActive} isPlaying={isPlaying} synth={synths}
+              // step={whichBox}
+            />
           )
         })}
         <button className="play-button" onClick={handlePlay}>{!isPlaying ? 'Play' : 'Stop'}</button>
